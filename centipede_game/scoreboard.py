2a2c8e483547882388ebc205cb92f3996b6743cc
Loc Truong <100035903+locke21@users.noreply.github.com>
2022-09-11 15:21:14 -0400

    Update scoreboard.py

--- centipede_game/scoreboard.py
+++ centipede_game/scoreboard.py
@@ -5,13 +5,14 @@ class Scoreboard(Turtle):
     def __init__(self):
         super(Scoreboard, self).__init__()
         self.current_score = 0
-        self.highest_score = 0
+        with open("data.txt") as file:
+            self.highest_score = int(file.read())
         self.hideturtle()
         self.penup()
         self.color('green')
         self.goto(-165, 260)
         self.write(f'Current Score: {self.current_score}       --       '
-                          f'Highest Score: {self.highest_score}', font=("ariel", 12, "bold"))
+                   f'Highest Score: {self.highest_score}', font=("ariel", 12, "bold"))
 
     def add_score(self):
         self.current_score += 1
@@ -23,6 +24,8 @@ class Scoreboard(Turtle):
 
     def update_score(self):
         self.highest_score = self.current_score if self.current_score >= self.highest_score else self.highest_score
+        with open("data.txt", "w") as change_score:
+            change_score.write(str(self.highest_score))
         self.clear()
         self.write(f'Current Score: {self.current_score}       --       '
-                          f'Highest Score: {self.highest_score}', font=("ariel", 12, "bold"))
+                   f'Highest Score: {self.highest_score}', font=("ariel", 12, "bold"))
